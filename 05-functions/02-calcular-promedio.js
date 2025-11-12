7e16cf91865964e725a2aca5143055fe2861198f
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-11-11 19:36:11 -0500

    [+] 02-calcular-promedio.js: Divide las tareas por cada funcion calcularPromedio() sumarNotas()

--- 05-functions/02-calcular-promedio.js
+++ 05-functions/02-calcular-promedio.js
@@ -1,18 +1,32 @@
-// 1. Captura de datos
+function calcularPromedio( total, cantidadNotas ) {
+    // Clases: Empiezan con la primera en Mayuscula
+    return ( total / cantidadNotas ).toFixed( 1 );  // Redondear mantiene la cantidad de decimales que se le indique
+    return Math.round( total / cantidadNotas );     // Redondear dinamicamente, al entero mas proximo
+    return Math.ceil(total / cantidadNotas);        // Redondear hacia arriba, al entero mas proximo
+    return Math.floor(total / cantidadNotas);       // Redondear hacia abajo, al entero mas proximo
+}
 
-// Funcion Declarativa
-function calcularPromedio( nombreAlumno, listaNotas ) {
-    // 2. Sumar las notas
+function sumarNotas ( listaNotas ) {
     let sumaNotas = 0;
     
     for( let nota of listaNotas ) {
         // console.log( nota );
         sumaNotas = sumaNotas + nota;
     }
-    
-    // 3. Calcular el promedio
-    const promedio = sumaNotas / notas.length;
-    
-    // 4. Imprimir el resultado
-    console.log( nombreAlumno, promedio );
+
+    return sumaNotas;  // Una funcion solo puede retornar un valor
 }
+
+// Implementacion:
+let estudiante1 = {
+    nombre: 'Gabriel',
+    notas: [ 3.4, 1.2, 3.7, 4.1 ]
+};
+
+// Inserta nueva nota
+estudiante1.notas.push( 3.3 );
+
+const totalNotas = sumarNotas( estudiante1.notas );
+const promedio = calcularPromedio( totalNotas, estudiante1.notas.length );
+
+console.log( 'La nota final de ', estudiante1.nombre, ' es ', promedio  );
