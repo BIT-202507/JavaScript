cc11909950ca0277932157ab155be807985643ba
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-11-12 20:59:33 -0500

    [+] Tarea en clase (Nov-12)

---
+++ 06-modules/t02/index.js
@@ -0,0 +1,14 @@
+// Calcular el IMC (Indice de Masa Corporal)
+//  Formula ==> IMC = peso(kg) / altura(m)^2.
+const pacientes = [
+    { nombre: 'Nilson', peso: 70, altura: 1.72 },
+    { nombre: 'Kevin', peso: 68, altura: 1.70 },
+    { nombre: 'Camilo', peso: 84, altura: 1.83 },
+    { nombre: 'Ronald', peso: 77, altura: 1.78 },
+    { nombre: 'Gabriel', peso: 58, altura: 1.66 },
+];
+
+// 1. Iterar la lista de pacientes
+// 1.1 De esta lista extraer el peso y la altura
+// 1.2 Pasar los valores extraidos a la funcion calcularIMC (formula)
+// 1.3 Listar nombre del paciente y el IMC 
