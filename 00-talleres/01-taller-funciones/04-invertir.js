0e5184317e22690d10b915d37a50c44fc4051325
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-11-18 19:07:54 -0500

    [+] Taller Funciones: 04.js

---
+++ 00-talleres/01-taller-funciones/04-invertir.js
@@ -0,0 +1,34 @@
+// 4. Invertir un array:
+// ○ Función invertirArray(array) que retorne un nuevo array invertido.
+// ○ Ejemplo: invertirArray(["a", "b", "c"]) → ["c", "b", "a"].
+
+// 1. Datos de Entrada
+// 1, 2, 3, 4
+//          array.length - 1
+
+// 2. Proceso Invierte los numeros de la lista
+
+// 3. Datos de salida
+// 4
+
+function invertirArray( array ) {
+    const arrayInvertido = [];
+    // console.log(array);
+
+    for( let i = array.length - 1; i >= 0; i-- ) {
+        // console.log( array[ i ] );
+        arrayInvertido.push( array[ i ] );
+    } 
+
+    return arrayInvertido;          //  [ 4, 3, 2, 1 ]
+}
+
+// Implementacion
+const listaNumeros = [1, 2, 3, 4];
+
+let resultado = invertirArray(listaNumeros);
+console.log( resultado );
+
+const listaLetras = ["a", "b", "c"];
+resultado = invertirArray( listaLetras );
+console.log( resultado );
